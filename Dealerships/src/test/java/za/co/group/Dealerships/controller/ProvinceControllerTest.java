7aa6efc308468d5bb2c242ffd3bdd99a5f5c9b94
Sonwabile Nomnganga <Sonwabile.Nomnganga@StandardBank.co.za>
2023-02-14 03:21:06 +0200

    added province controller test

--- Dealerships/src/test/java/za/co/group/Dealerships/controller/ProvinceControllerTest.java
+++ Dealerships/src/test/java/za/co/group/Dealerships/controller/ProvinceControllerTest.java
@@ -1,15 +1,41 @@
 package za.co.group.Dealerships.controller;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.springframework.test.util.ReflectionTestUtils;
+import za.co.group.Dealerships.domain.Country;
+import za.co.group.Dealerships.service.CountryService;
+import za.co.group.Dealerships.service.ProvinceService;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.atLeastOnce;
+import static org.mockito.Mockito.verify;
+import static org.mockito.MockitoAnnotations.initMocks;
 
-class ProvinceControllerTest {
+public class ProvinceControllerTest {
 
-    @BeforeEach void setUp() {
+    private ProvinceController provinceController;
+    @Mock
+    private CountryService countryService;
+
+    @Mock
+    private ProvinceService provinceService;
+
+    @Before
+    public void setUp() {
+        initMocks(this);
+        provinceController = new ProvinceController();
+        ReflectionTestUtils.setField(provinceController, "countryService", countryService);
+        ReflectionTestUtils.setField(provinceController, "provinceService", provinceService);
     }
 
-    @Test void getProvincesByCountryCode() {
+    @Test
+    public void getProvincesByCountryCode_testIfAllDependenciesAreUsed() {
+
+        provinceController.getProvincesByCountryCode(any(String.class));
+
+        verify(countryService, atLeastOnce()).getByCountryCode(any(String.class));
+        verify(provinceService, atLeastOnce()).getByCountryCode(any(Country.class));
     }
 }
